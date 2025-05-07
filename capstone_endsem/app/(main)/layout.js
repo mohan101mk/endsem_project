c9f32e134c95b899bfe5c5985bbb783c553c462e
Mohan Kumar C R <mohanmk@Mohans-MacBook-Pro.local>
2025-05-07 13:20:55 +0530

    added footer

--- capstone_endsem/app/(main)/layout.js
+++ capstone_endsem/app/(main)/layout.js
@@ -1,10 +1,12 @@
 import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 export default function MainLayout({ children }) {
   return (
-    <div>
+    <div style={{minHeight: '100vh', display:'flex' , flexDirection:'column'}}>
       <Navbar/>
       <main>{children}</main>
+      <Footer/>
     </div>
   );
 }
---
+++ capstone_endsem/app/components/footer.js
@@ -0,0 +1,30 @@
+import styles from './footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.text}>
+      © 2025 Budget Tracker — Made with ❤️ by MK
+      </div>
+      <div className={styles.links}>
+        <a
+          href="https://github.com/mohan101mk"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          GitHub
+        </a>
+        <span className={styles.separator}>|</span>
+        <a
+          href="https://www.linkedin.com/in/mohankumarcr"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          linkedin
+        </a>
+      </div>
+    </footer>
+  );
+}
